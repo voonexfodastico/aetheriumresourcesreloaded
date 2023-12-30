49a3c5954da5bed2259cb738d89dd8e171f667a6
voonexfodastico <aethe@DESKTOP-OFUTTRF>
2023-12-30 14:25:18 -0300

    fix

--- src/main/java/net/mcreator/aetheriumresourcesreloaded/procedures/CorruptedEffectOnEffectActiveTickProcedure.java
+++ src/main/java/net/mcreator/aetheriumresourcesreloaded/procedures/CorruptedEffectOnEffectActiveTickProcedure.java
@@ -10,20 +10,16 @@ import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.sounds.SoundSource;
-import net.minecraft.server.level.ServerLevel;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.core.BlockPos;
-
-import net.mcreator.aetheriumresourcesreloaded.AetheriumResourcesReloadedMod;
 
 public class CorruptedEffectOnEffectActiveTickProcedure {
 	public static void execute(LevelAccessor world, double x, double y, double z, Entity entity) {
 		if (entity == null)
 			return;
-		if (world instanceof ServerLevel _level)
-			_level.sendParticles(ParticleTypes.SMOKE, x, y, z, 5, 0.5, 0.5, 0.5, 1);
-		AetheriumResourcesReloadedMod.queueServerWork(80, () -> {
+		double efeito = 0;
+		efeito = efeito + 1;
+		if (efeito == 80) {
 			if (world instanceof Level _level) {
 				if (!_level.isClientSide()) {
 					_level.playSound(null, new BlockPos(x, y, z), ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("aetherium_resources_reloaded:glitching")), SoundSource.NEUTRAL, 1, 1);
@@ -31,9 +27,11 @@ public class CorruptedEffectOnEffectActiveTickProcedure {
 					_level.playLocalSound(x, y, z, ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("aetherium_resources_reloaded:glitching")), SoundSource.NEUTRAL, 1, 1, false);
 				}
 			}
-			entity.hurt(DamageSource.DRY_OUT, 4);
+			if (entity instanceof LivingEntity _entity)
+				_entity.hurt(new DamageSource("corruption").bypassArmor(), 1);
 			if (entity instanceof LivingEntity _entity && !_entity.level.isClientSide())
 				_entity.addEffect(new MobEffectInstance(MobEffects.LEVITATION, 40, 1, false, false));
-		});
+			efeito = 0;
+		}
 	}
 }
